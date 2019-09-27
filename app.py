07eae5406ee76c97354ca1283e9c5b3ebf5501b9
Loren Heubert-Aubry <lorenha@umich.edu>
2019-09-26 20:13:04 -0400

    attempting to run server when frozen

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    manager.run(host='0.0.0.0', port=port)
+    manager.run()
+    #app.run(port=8000)
